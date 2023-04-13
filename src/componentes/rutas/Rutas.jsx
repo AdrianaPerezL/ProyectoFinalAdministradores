dbbed14a766feeb5c40f7c65ad913678bb422ea7
AdrianaPerezL <112712237+AdrianaPerezL@users.noreply.github.com>
2023-04-13 09:13:11 -0600

    a

--- src/componentes/rutas/Rutas.jsx
+++ src/componentes/rutas/Rutas.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "../Dashboard";
-import Navbar from "../Navbar";
-import Sidebar from "../Sidebar";
+import IniciarSesionAdmi from "../IniciarSesion";
+import RegistroAdmi from "../RegistroAdmi"
+
 import "../../assets/styles/sidebar.css";
 import "bootstrap/js/dist/dropdown";
 import "bootstrap/js/dist/collapse";
@@ -11,86 +12,13 @@ import "bootstrap/js/dist/collapse";
 export const Rutas = () => {
   return (
     <>
-      <div className="container-fluid min-vh-100 bodyDashboard">
-        <div className="row ">
-          {toggle && (
-            <div
-              className="col-4 col-md-2 vh-100 position-fixed"
-              style={{ backgroundColor: "#572AB0" }}
-            >
-              <Sidebar />
-            </div>
-          )}
-          {toggle && <div className="col-4 col-md-2"></div>}
-          <div className="col">
-            <BrowserRouter>
-              <nav className="navbar navbar-expand-sm navbar-dark bg-transparent">
-                {toggle ? (
-                  <FontAwesomeIcon
-                    className="navbar-brand fs-4 ml-1"
-                    icon={faBars}
-                    onClick={Toggle}
-                  />
-                ) : (
-                  <FontAwesomeIcon
-                    className="navbar-brand fs-4 ml-1"
-                    icon={faCompass}
-                    onClick={Toggle}
-                  />
-                )}
-
-                <button
-                  className="navbar-toggler d-lg-none"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapsibleNavId"
-                  aria-controls="collapsibleNavId"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <FontAwesomeIcon
-                    className="navbar-brand fs-4 ml-1"
-                    icon={faBars}
-                  />
-                </button>
-                <div className="collapse navbar-collapse" id="collapsibleNavId">
-                  <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-                    <li className="nav-item dropdown">
-                      <a
-                        className="nav-link dropdown-toggle text-white"
-                        href="#"
-                        id="dropdownId"
-                        data-bs-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                      >
-                        Admin
-                      </a>
-                      <div
-                        className="dropdown-menu"
-                        aria-labelledby="dropdownId"
-                      >
-                        <a className="dropdown-item" href="#">
-                          Profile
-                        </a>
-                        <a className="dropdown-item" href="#">
-                          Setting
-                        </a>
-                        <a className="dropdown-item" href="#">
-                          Logout
-                        </a>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-              <Routes>
-                <Route path="/" element={<Dashboard />}></Route>
-              </Routes>
-            </BrowserRouter>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<IniciarSesionAdmi />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/registro" element={<RegistroAdmi />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
