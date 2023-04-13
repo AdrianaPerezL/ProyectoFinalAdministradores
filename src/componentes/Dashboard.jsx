31de2784c645a0f98b453a8265e80cd090121055
AdrianaPerezL <112712237+AdrianaPerezL@users.noreply.github.com>
2023-04-13 09:10:45 -0600

    h

--- src/componentes/Dashboard.jsx
+++ src/componentes/Dashboard.jsx
@@ -6,6 +6,45 @@ import '../assets/styles/dashboard.css'
 
 
 function Dashboard() {
+
+    let ctxL = document.getElementById("lineChart").getContext('2d');
+
+let myLineChart = new Chart(ctxL, {
+  type: 'line',
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+      label: "Femenino",
+      
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        'rgba(105, 0, 132, .2)',
+      ],
+      borderColor: [
+        'rgba(200, 99, 132, .7)',
+      ],
+      borderWidth: 2,
+      fontFamily: 'Poppins',
+    },
+    {
+      label: "Masculino",
+      data: [28, 48, 40, 19, 86, 27, 90],
+      backgroundColor: [
+        'rgba(0, 137, 132, .2)',
+      ],
+      borderColor: [
+        'rgba(0, 10, 130, .7)',
+      ],
+      borderWidth: 2
+    }
+    ]
+  },
+  options: {
+    responsive: true,
+    fill: true,
+    
+  }
+});
     return (
         <div>
             <Navbar />
